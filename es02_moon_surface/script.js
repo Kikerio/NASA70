8434a067dfdf9680d57fbe2415a833eab1d06aba
Michelle Chicherio <michelle.chicherio@gmail.com>
2026-03-05 15:42:58 +0100

    .

---
+++ es02_moon_surface/script.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400)
+
+}
+
+function draw() {
+    background(220, 0, 0)
+
+    for(let i=0; i<6; i = i++) {
+        point(i * 10, i * 10)
+    }
+
+    // while
+
+    // point(10,10)
+    // point(20,10)
+    // point(30,10)
+    // point(40,10)
+    // point(50,10)
+    // point(60,10)
+    
+}
